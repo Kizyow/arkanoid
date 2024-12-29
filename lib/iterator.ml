2c9eac27ae76087a53c549d9ed801e969b1a0e80
Pierre S <axiolerre@gmail.com>
2024-12-30 00:33:53 +0100

    Ajout de map3 à l'itérateur pour gérer la construction du flux avec l'accélération en plus dedans

--- lib/iterator.ml
+++ lib/iterator.ml
@@ -12,6 +12,7 @@ module type Intf = sig
   val uncons : 'a t -> ('a * 'a t) option
   val apply : ('a -> 'b) t -> 'a t -> 'b t
   val map2 : ('a -> 'b -> 'c) -> 'a t -> 'b t -> 'c t
+  val map3 : ('a -> 'b -> 'c -> 'd) -> 'a t -> 'b t -> 'c t -> 'd t
 end
 
 type 'a flux = Tick of ('a * 'a flux) option Lazy.t
@@ -57,4 +58,5 @@ module Flux : Intf with type 'a t = 'a flux = struct
   (* implantation rapide mais inefficace de map *)
   let map f i = apply (constant f) i
   let map2 f i1 i2 = apply (apply (constant f) i1) i2
+  let map3 f i1 i2 i3 = apply (map2 f i1 i2) i3
 end
